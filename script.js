39bd522a25d19ee672057630883fece52e8ce625
Pedro <pedrosteinmuller10105@hotmail.com>
2022-07-13 18:33:28 -0300

    resolucao em conjunto requisito 18

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const button = document.querySelector('#btn');
 const email = document.getElementById('emailPlace');
 const password = document.getElementById('passwordPlace');
+const checkBtn = document.getElementById('agreement');
+const subBtn = document.getElementById('submit-btn');
 
 button.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+checkBtn.addEventListener('click', () => {
+  if (checkBtn.checked === true) {
+    subBtn.disabled = false;
+  } else {
+    subBtn.disabled = true;
   }
 });
