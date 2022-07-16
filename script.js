d95620757cf931833c5eac1da8b0c0e542a10c35
Pedro <pedrosteinmuller10105@hotmail.com>
2022-07-16 19:44:43 -0300

    resolucao e reformulação dos codigos do requiso bonus 21

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const button = document.getElementById('btn');
-const email = document.getElementById('emailPlace');
+const headerBtn = document.getElementById('btn'); // modificado button --> headerBtn
+const headerEmail = document.getElementById('emailPlace');
 const password = document.getElementById('passwordPlace');
 const checkBtn = document.getElementById('agreement');
 const subBtn = document.getElementById('submit-btn');
 const dispCount = document.querySelector('#counter');
 const counterText = document.querySelector('#textarea');
+const formBtn = document.getElementById('submit-btn'); // linha adicionada
 
-button.addEventListener('click', () => {
-  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+headerBtn.addEventListener('click', () => { // modificado button --> headerBtn
+  if (headerEmail.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
@@ -26,4 +27,37 @@ counterText.addEventListener('input', () => {
   let count = counterText.textLength;
   count = 500 - count;
   dispCount.innerText = count;
+});
+
+function fetchFormData() {
+  const checkedSubjects = document.querySelectorAll('.subject:checked');
+  const subjectList = [];
+  for (let i = 0; i < checkedSubjects.length; i += 1) {
+    subjectList.push(checkedSubjects[i].value);
+  }
+  const name = document.getElementById('input-name').value;
+  const lastname = document.getElementById('input-lastname').value;
+  const fullName = `Nome: ${name} ${lastname}`;
+  const email = `Email: ${document.getElementById('input-email').value}`;
+  const house = `Casa: ${document.getElementById('house').value}`;
+  const family = `Família: ${document.querySelector('input[name="family"]:checked').value}`;
+  const subjects = `Matérias: ${subjectList.join(', ')}`;
+  const rate = `Avaliação: ${document.querySelector('input[name="rate"]:checked').value}`;
+  const obs = `Observações: ${document.getElementById('textarea').value}`;
+  const data = [fullName, email, house, family, subjects, rate, obs];
+  return data;
+}
+
+formBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const formData = fetchFormData();
+  const main = document.getElementsByTagName('main')[0];
+  main.firstElementChild.style.display = 'none';
+  const newForm = document.getElementById('form-data');
+  for (let i = 0; i < 7; i += 1) {
+    const newTagP = document.createElement('p');
+    newTagP.style.margin = '5px 0 0 10px';
+    newForm.appendChild(newTagP);
+    newTagP.innerText = formData[i];
+  }
 });
